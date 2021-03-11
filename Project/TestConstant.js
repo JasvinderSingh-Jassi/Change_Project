90873f8235606c860c7d87eb291911e1bf5c5b10
JasvinderSingh-Jassi <51856951+JasvinderSingh-Jassi@users.noreply.github.com>
2021-03-11 20:48:19 +0530

    Add files via upload

---
+++ Project/TestConstant.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let tc = function () {
+
+    this.URL = "https://www.globalsqa.com/angularjs-protractor-practice-site/";
+    this.Name = "Jasvinder Singh";
+    this.Email = "jasvinder1997@gmail.com";
+
+}
+
+module.exports = new tc();
