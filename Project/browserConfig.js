a4d3ac5fb4eb2959f0ae785f2c0037432669e8e9
JasvinderSingh-Jassi <51856951+JasvinderSingh-Jassi@users.noreply.github.com>
2021-04-05 13:20:45 +0530

    Add files via upload

---
+++ Project/browserConfig.js
@@ -0,0 +1,23 @@
+const chrome = {
+    'browserName': 'chrome',
+
+};
+
+const firefox = {
+    'browserName': 'firefox',
+};
+
+const firefox_chrome = [{
+        'browserName': 'chrome'
+    },
+    {
+        'browserName': 'firefox'
+    },
+];
+
+
+module.exports = {
+    chrome,
+    firefox,
+    firefox_chrome
+}
